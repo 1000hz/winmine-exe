31a7d4d3f6b2799b1eb405d03640de45ddba6f64
Cina Saffary <cinasaffary@gmail.com>
2018-11-15 03:19:49 -0600

    updating packages, adding test:debug task, getting next to transpile lodash

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    development: {
+      presets: ["next/babel"]
+    },
+    production: {
+      presets: ["next/babel"]
+    },
+    test: {
+      presets: [["@babel/preset-env"], "@babel/preset-react"]
+    }
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 const Uglify = require("uglifyjs-webpack-plugin")
+const withTM = require("next-plugin-transpile-modules")
+
+const transpileOptions = withTM({
+  transpileModules: ["lodash-es"]
+})
+
+module.exports = transpileOptions
 
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     webpack: function(c) {
-      c.plugins = c.plugins.filter(
-        plugin => plugin.constructor.name !== "UglifyJsPlugin"
-      )
+      c.plugins = c.plugins.filter(plugin => plugin.constructor.name !== "UglifyJsPlugin")
 
       c.plugins.push(new Uglify())
 
       return c
-    }
+    },
+    ...transpileOptions
   }
 }
