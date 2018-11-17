47c7b5fe1bc035a3da6099807243466f80d266cf
Cina Saffary <cinasaffary@gmail.com>
2018-11-16 23:02:41 -0600

    adding support for disabling a menubar item

--- components/_ui/Window.js
+++ components/_ui/Window.js
@@ -88,9 +88,13 @@ const MenuBarItem = styled.button.attrs({
   font-family: ${props => props.theme.fontFamilies.default};
   outline: none;
 
-  :hover {
+  :hover:not(:disabled) {
     color: ${props => props.theme.colors.gray[3]};
     background: ${props => props.theme.colors.navy};
+  }
+
+  :disabled {
+    color: ${props => props.theme.colors.gray[1]};
   }
 `
 
