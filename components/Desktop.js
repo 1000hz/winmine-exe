c31dbd4ce20156e772b3625d94c4c22702d6a7af
Cina Saffary <cinasaffary@gmail.com>
2019-02-10 23:20:39 -0600

    refactoring Desktop a bit - adding a repo link - updating useEventListener call to new API - refactoring the way Icons are rendered - adjusting icon spacing

--- components/Desktop.js
+++ components/Desktop.js
@@ -1,6 +1,6 @@
-import React, {useCallback} from "react"
+import React from "react"
 import styled from "styled-components"
-import DesktopIcon from "~/components/_ui/DesktopIcon"
+import {IconWithLabel, ApplicationIcon} from "~/components/_ui/DesktopIcon"
 import Taskbar from "~/components/_ui/Taskbar/Taskbar"
 import Minesweeper from "~/components/Minesweeper"
 import About from "~/components/About"
@@ -9,24 +9,34 @@ import {useTaskManager} from "~/lib/useTaskManager"
 
 const StyledDesktop = styled.div`
   display: grid;
-  grid-auto-columns: 64px;
-  grid-auto-rows: 64px;
-  padding: 2px 4px;
+  grid-auto-columns: 75px;
+  grid-auto-rows: 75px;
+  padding: 4px 0;
 `
 
-const appShortcuts = [Minesweeper, About]
+const icons = (
+  <>
+    <ApplicationIcon application={Minesweeper} />
+    <ApplicationIcon application={About} />
+    <IconWithLabel
+      icon={require("~/components/About/images/github.png")}
+      title="GitHub Repo"
+      onOpen={() => window.open("https://github.com/1000hz/winmine-exe")}
+    />
+  </>
+)
 
 const Desktop = ({children}) => {
   const {tasks, createTask} = useTaskManager()
-  useEventListener("contextmenu", useCallback(e => e.preventDefault(), []))
+  useEventListener(global.document, "contextmenu", e => e.preventDefault())
 
   return (
     <>
       <StyledDesktop>
-        {appShortcuts.map((app, id) => <DesktopIcon key={id} application={app} />)}
         {Object.values(tasks).map(task => (
           <task.application.AppComponent key={task.id} {...task} />
         ))}
+        {icons}
       </StyledDesktop>
       <Taskbar tasks={tasks} />
     </>
